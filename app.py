cff9705fe45343b05590747a4969f534ba1d0efc
Jeff <jzqiu@arista.com>
2023-05-31 15:51:25 -0500

    Add health check route

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+#!/usr/bin/env python                                                                                                                                                                        
+# Copyright (c) 2023 Arista Networks, Inc.  All rights reserved.                                                                                                                             
+# Arista Networks, Inc. Confidential and Proprietary.
+
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-   return "hello world!"
+def result( c, code=200 ):
+   r = jsonify( c )
+   r.status_code = code
+   return r
+
+@app.route( '/' )
+def helloWorld():
+   return result( 'hello world!' )
+
+@app.route( '/health', methods=[ 'GET' ] )
+def health():
+   return result( { 'status': 'Alive!' } )
 
 if __name__ == "__main__":
    app.run(debug=True)
